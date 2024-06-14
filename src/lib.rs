9227f5c00ec803617b955f0c1d0a732ebdda403d
Kyle Barry <kyle@qauntsolutions.co.za>
2024-06-14 11:19:49 +0200

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![feature(coroutines, coroutine_trait, stmt_expr_attributes)]
 #![feature(unboxed_closures)]
 
-use std::marker::PhantomData;
 use std::ops::{Coroutine, CoroutineState};
 use std::pin::Pin;
 use std::sync::Arc;
@@ -12,11 +11,50 @@ use std::time::Duration;
 use crossbeam_channel::{Receiver, unbounded};
 
 
-struct Inlet {
+/// Receives data from an external source and broadcasts the data via channels.
+///
+/// Using an `Inlet` yields the following benefits:
+/// - Abstracts away the use of channels. You only need to write a coroutine that takes a parameter
+/// and yields a result.
+/// - The coroutine does not have to be aware of termination signals, or joining threads. This
+/// functionality is handled by `Inlet`.
+/// - Easy integration with other `Reflux` modules.
+///
+/// # Example
+/// ```
+///  #![feature(coroutines, coroutine_trait, stmt_expr_attributes)]
+///  #![feature(unboxed_closures)]
+///  use std::sync::Arc;
+///  use std::sync::atomic::{AtomicBool, Ordering};
+///  use crossbeam_channel::Receiver;
+///  use reflux::add_routine;
+///  use reflux::Inlet;
+///  let stop_flag = Arc::new(AtomicBool::new(false));
+///  let (inlet, inlet_chan): (Inlet, Receiver<String>) = Inlet::new(
+///      add_routine!(#[coroutine] |_: ()| {
+///          yield "Hello, world".to_string()
+///      }), stop_flag.clone());
+///
+///  let data = inlet_chan.recv().unwrap();
+///  stop_flag.store(true, Ordering::Relaxed);
+///  inlet.join().unwrap();
+///
+///  assert_eq!(data, "Hello, world".to_string())
+/// ```
+pub struct Inlet {
     inlet_fn: JoinHandle<()>,
 }
 
 impl Inlet {
+    /// Creates a new `Inlet` object.
+    /// 
+    /// # Parameters
+    /// - `inlet_fn` - A coroutine that reads and returns data from an external source.
+    /// The use of the `add_routine!` macro is necessary when passing in an `inlet_fn`.
+    /// - `stop_sig` - A flag to signal the `Inlet` object to terminate
+    /// 
+    /// # Returns
+    /// A `Inlet` object and a `Receiver` to receive data from the `inlet_fn`
     pub fn new<F, C, T>(inlet_fn: F, stop_sig: Arc<AtomicBool>) -> (Self, Receiver<T>)
         where 
             F: Fn() -> C + Send + 'static,
@@ -43,28 +81,71 @@ impl Inlet {
 
         (s, rx)
     }
+    
+    /// Waits for the `Inlet` object to finish execution
     pub fn join(self) -> thread::Result<()> {
         self.inlet_fn.join()?;
         Ok(())
     }
 }
 
-
-struct Outlet {
+/// An object that receives data from a `Reflux` network and sends the data to some external sink.
+/// 
+/// Using an `Outlet` yields the following benefits:
+/// - Abstracts away the use of channels. You only need to receive a parameter and send it to an
+/// external sink
+/// - The function does not have to be aware of termination signals, or joining threads. This
+/// functionality is handled by `Outlet`.
+/// - Easy integration with other `Reflux` modules.
+/// 
+/// # Example
+/// ```
+/// use reflux::Outlet;
+///  use std::sync::Arc;
+///  use std::sync::atomic::{AtomicBool, Ordering};
+///  use crossbeam_channel::Receiver;
+///  use reflux::add_routine;
+///  use crossbeam_channel::unbounded;
+/// 
+/// let stop_flag = Arc::new(AtomicBool::new(false));
+/// let (test_tx, test_rx) = unbounded();
+/// let (data_tx, data_rx) = unbounded();
+/// let outlet= Outlet::new(move |test: String| {
+/// test_tx.send(test).unwrap();
+/// eprintln!("fok");
+/// }, data_rx, stop_flag.clone());
+/// 
+/// data_tx.send("Hello, world".to_string()).unwrap();
+/// 
+/// let data_recv = test_rx.recv().unwrap();
+/// stop_flag.store(true, Ordering::Relaxed);
+/// outlet.join().unwrap();
+/// 
+/// assert_eq!(data_recv, "Hello, world".to_string())
+/// ```
+pub struct Outlet {
     outlet_fn: JoinHandle<()>
 }
 
 impl Outlet {
+    /// Creates a new `Outlet` object.
+    ///
+    /// # Parameters
+    /// - `outlet_fn` - A function that receives data from a `Reflux` network and sends it to an
+    /// external sink.
+    /// - `receiver` - A `Receiver` channel object from which to receive data.
+    /// - `stop_sig` - A flag to signal the `Inlet` object to terminate
+    ///
+    /// # Returns
+    /// A `Outlet` object
     pub fn new<T, F>(outlet_fn: F, receiver: Receiver<T>, stop_sig: Arc<AtomicBool>) -> Self
         where
             T: Send + 'static,
             F: Fn(T) + Send + 'static {
         let outlet = thread::spawn(move || {
-            loop {
-                while !stop_sig.load(Ordering::Relaxed) {
-                    if let Ok(data) = receiver.recv_timeout(Duration::from_millis(10)) {
-                        outlet_fn(data)
-                    }
+            while !stop_sig.load(Ordering::Relaxed) {
+                if let Ok(data) = receiver.recv_timeout(Duration::from_millis(10)) {
+                    outlet_fn(data)
                 }
             }
         });
@@ -73,16 +154,15 @@ impl Outlet {
         }
     }
     
+    /// Waits for the `Outlet` object to finish execution
     pub fn join(self) -> thread::Result<()> {
         self.outlet_fn.join()?;
         Ok(())
     }
 }
 
-
-
-
 /// A simple macro to create a function that returns a coroutine.
+#[macro_export]
 macro_rules! add_routine {
     ($a: expr) => {
         move || {
@@ -112,7 +192,7 @@ mod tests {
         
         assert_eq!(data, "Hello, world".to_string())
     }
-    
+
     #[test]
     fn outlet_works() {
         let stop_flag = Arc::new(AtomicBool::new(false));
@@ -120,14 +200,15 @@ mod tests {
         let (data_tx, data_rx) = unbounded();
         let outlet= Outlet::new(move |test: String| {
             test_tx.send(test).unwrap();
+            eprintln!("fok");
         }, data_rx, stop_flag.clone());
-        
+
         data_tx.send("Hello, world".to_string()).unwrap();
-        
+
         let data_recv = test_rx.recv().unwrap();
         stop_flag.store(true, Ordering::Relaxed);
         outlet.join().unwrap();
-        
+
         assert_eq!(data_recv, "Hello, world".to_string())
     }
 
